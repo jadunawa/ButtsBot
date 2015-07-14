adcef88b02a1aa2dbfc193608fecfc2f6dadd275
Judson Dunaway-Barlow <judson.db@gmail.com>
2015-07-14 11:20:12 -0400

    Bot now doesnt respond to itself!

--- buttsbot.py
+++ buttsbot.py
@@ -11,8 +11,7 @@ top10 = r.get_subreddit('astros').get_hot(limit=10)
 
 already_checked = []  # make list of comment permalinks
 
-keywords = ['butt', 'butts', 'booty', 'buttcheeks', 'keyster',
-            'heinie']  # make list of words to trigger the comment reply
+keywords = ['butt', 'butts', 'booty', 'buttcheeks', 'keyster', 'heinie']  # make list of words to trigger the comment reply
 # TODO: Get a list of a bunch of imgur links to Astros Butts
 
 reply_string = '#THIS BOT IS STILL IN DEVELOPMENT\n\nYou have activated the Astros\' buttsbot! Here is a picture of an [Astros butt!](http://www.rantsports.com/mlb/files/2014/02/Jason-Castro-Houston-Astros.jpg) Thanks for enjoying Astros buttocks! Go \'Stros!\n\nAny problems with this bot? Please send a message to /u/jdb12.'
@@ -23,32 +22,28 @@ while True:
     i = 1
     # go through top 20 submissions of /r/Astros
     for submission in subreddit.get_hot(limit=20):
-        print('Testing submission ', i)
         submission_date = datetime.fromtimestamp(submission.created_utc)  # get submission date
         day_ago = datetime.fromtimestamp(time.time() - (24 * 60 * 60))  # find date for 24 hours ago
         # ignore posts over 24 hours old
-        if (int((day_ago - submission_date).days) < 2):
+        if (int((day_ago - submission_date).days) + 1< 2):
+            print('Testing submission ', i)
             print(day_ago - submission_date).days
-            sys.exit(0)
             submission.replace_more_comments(limit=None, threshold=0)  # get all comments instead of just first few
             all_comments = submission.comments  # make array of comments
             flaternized_comments = praw.helpers.flatten_tree(all_comments)  # ignore tree structure of comments
             # Iterate through comments in the submission
             for comment in flaternized_comments:
-                # TODO: Ignore comments over 24 hours old
                 # Ignore comments that have already been checked to avoid multiple replies to the same comment
                 if (comment.permalink not in already_checked):
                     l_comment = str(comment).lower()  # make the comment lowercase
-                    talks_about_butts = any(
-                        string in l_comment for string in keywords)  # set up boolean for talking about buts
-                    # print comment.created_utc
-                    # TODO: Make sure it doesn't respond to itself
-                    # If it talks about butts, respond with the correct string
-                    if talks_about_butts:
+                    talks_about_butts = any(string in l_comment for string in keywords)  # set up boolean for talking about butts
+                    # If the comment talks about butts and isn't a comment by this bot, respond with the correct string
+                    if talks_about_butts and str(comment.author)!="buttsbot":
+                        print("Comment author: "+str(comment.author))
                         # TODO: Randomize which imgur link is posted as part of the reply string using a new function
                         # comment.reply(reply_string) #reply to the comment
-                        print("Replied to a comment")
+                        print("Replied to a comment: " +str(comment.permalink))
                     already_checked.append(comment.permalink)  # add comment to already_checked
+            print "-----------------------------------------------------------------"
         i += 1
-        print "-----------------------------------------------------------------"
     time.sleep(5)  # sleep for a little bit to allow time for new comments to be made
