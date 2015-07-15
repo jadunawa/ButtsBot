b7caeedab8a1e847dc444c669e7d83f144d41daa
Judson Dunaway-Barlow <judson.db@gmail.com>
2015-07-15 08:22:05 -0400

    Added more keywords and added new butt link to a new array (randomness has not been implemented yet)

--- buttsbot.py
+++ buttsbot.py
@@ -27,7 +27,8 @@ top10 = r.get_subreddit('astros').get_hot(limit=10)
 
 already_checked = []  # make list of comment permalinks
 
-keywords = ['butt', 'booty', 'buttcheeks', 'keyster', 'heinie', 'derriere', 'posterior']  # make list of words to trigger the comment reply
+keywords = ['butt', 'booty', 'ass', 'keyster', 'heinie', 'derriere', 'posterior', 'arse', 'bottom', 'tush', 'rear', 'bum', 'caboose']  # make list of words to trigger the comment reply
+butt_links=['http://imgur.com/Vkx6fMI']
 # TODO: Get a list of a bunch of imgur links to Astros Butts
 
 reply_string = 'You have activated the Astros\' buttsbot! Here is a picture of an [Astros butt!](http://www.rantsports.com/mlb/files/2014/02/Jason-Castro-Houston-Astros.jpg) Thanks for enjoying Astros buttocks! Go \'Stros!\n\nAny problems with this bot? Please send me a message!'
