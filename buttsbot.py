c620282272bdc039946a817755e8205ede782538
Judson Dunaway-Barlow <judson.db@gmail.com>
2015-07-21 16:10:41 -0400

    Changed one link to go directly to image

--- buttsbot.py
+++ buttsbot.py
@@ -33,7 +33,7 @@ already_checked = []  # make list of comment permalinks
 #TODO: Work on regex
 keywords = ['butt', 'booty', ' ass ', 'ass.' 'keyster', 'heinie', 'hiney', 'derriere', 'posterior', 'arse', 'bottom', 'tush', 'rear', 'bum', 'caboose', 'rump', 'fanny', 'glutes']  # make list of words to trigger the comment reply
 # TODO: Get a list of a bunch of imgur links to Astros Butts
-butt_links=['Bagwell butt!](http://imgur.com/Vkx6fMI', 'Castro butt!](http://www.rantsports.com/mlb/files/2014/02/Jason-Castro-Houston-Astros.jpg', 'Lowrie butt!](http://i.imgur.com/TwTi4DT.jpg',
+butt_links=['Bagwell butt!](http://imgur.com/Vkx6fMI.jpg', 'Castro butt!](http://www.rantsports.com/mlb/files/2014/02/Jason-Castro-Houston-Astros.jpg', 'Lowrie butt!](http://i.imgur.com/TwTi4DT.jpg',
             'Conger butt!](http://i.imgur.com/P5C2BGK.jpg', 'Carlos Lee butt!](http://i.imgur.com/G5ZzVmp.jpg', 'McHugh butt!](http://i.imgur.com/hClbvuL.jpg', 'Ausmus and Berkman butt!](http://i.imgur.com/mL5TtMx.jpg']
 
 subreddit = r.get_subreddit('Astros')  # get /r/Astros
@@ -62,7 +62,7 @@ while True:
                     # If the comment talks about butts and isn't a comment by this bot, respond with the correct string
                     if talks_about_butts and str(comment.author)!="buttsbot":
                         print("Comment author: "+str(comment.author))
-                        butt_number=random.randint(0,len(butt_links))
+                        butt_number=random.randint(0,len(butt_links)-1)
                         full_reply="You have activated the Astros buttsbot! Here is a picture of [{}) Thanks for enjoying Astros buttocks! Go \'Stros!\n\nAny problems with this bot? Please send me a message!".format(butt_links[butt_number])
                         comment.reply(full_reply) #reply to the comment
                         print("Replied to a comment: " +str(comment.permalink))
