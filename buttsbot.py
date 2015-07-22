ddaabc38033e9e481f3828927f28371e874e16f0
Judson Dunaway-Barlow <judson.db@gmail.com>
2015-07-22 16:53:36 -0400

    End of day database update

--- buttsbot.py
+++ buttsbot.py
@@ -34,7 +34,8 @@ already_checked = []  # make list of comment permalinks
 keywords = ['butt', 'booty', ' ass ', 'ass.' 'keyster', 'heinie', 'hiney', 'derriere', 'posterior', 'arse', 'bottom', 'tush', 'rear', 'bum', 'caboose', 'rump', 'fanny', 'glutes']  # make list of words to trigger the comment reply
 # TODO: Get a list of a bunch of imgur links to Astros Butts
 butt_links=['Bagwell butt!](http://imgur.com/Vkx6fMI.jpg', 'Castro butt!](http://www.rantsports.com/mlb/files/2014/02/Jason-Castro-Houston-Astros.jpg', 'Lowrie butt!](http://i.imgur.com/TwTi4DT.jpg',
-            'Conger butt!](http://i.imgur.com/P5C2BGK.jpg', 'Carlos Lee butt!](http://i.imgur.com/G5ZzVmp.jpg', 'McHugh butt!](http://i.imgur.com/hClbvuL.jpg', 'Ausmus and Berkman butt!](http://i.imgur.com/mL5TtMx.jpg']
+            'Conger butt!](http://i.imgur.com/P5C2BGK.jpg', 'Carlos Lee butt!](http://i.imgur.com/G5ZzVmp.jpg', 'McHugh butt!](http://i.imgur.com/hClbvuL.jpg', 'Ausmus and Berkman butt!](http://i.imgur.com/mL5TtMx.jpg',
+            'Minor Leagues Lowrie butt!](http://i.imgur.com/VTxAaqn.jpg']
 
 subreddit = r.get_subreddit('Astros')  # get /r/Astros
 
@@ -58,12 +59,12 @@ while True:
                 if (str(c.execute("SELECT link FROM permalinks WHERE link='{}'".format(perma)).fetchone())=="None"):
                     print "NONE OMG THIS WORKS"
                     l_comment = str(comment).lower()  # make the comment lowercase
-                    talks_about_butts = any(string in l_comment for string in keywords)  # set up boolean for talking about butts
+                    talks_about_butts = any(string in l_comment for string in keywords)  #boolean for talking about butts
                     # If the comment talks about butts and isn't a comment by this bot, respond with the correct string
                     if talks_about_butts and str(comment.author)!="buttsbot":
                         print("Comment author: "+str(comment.author))
                         butt_number=random.randint(0,len(butt_links)-1)
-                        full_reply="You have activated the Astros buttsbot! Here is a picture of [{}) Thanks for enjoying Astros buttocks! Go \'Stros!\n\nAny problems with this bot? Please send me a message!".format(butt_links[butt_number])
+                        full_reply="You have activated the Astros buttsbot! Here is a picture of [{}) Thanks for enjoying Astros buttocks! Go \'Stros!\n\nAny problems with this bot? Suggestions for more butts? Please send me a message!".format(butt_links[butt_number])
                         comment.reply(full_reply) #reply to the comment
                         print("Replied to a comment: " +str(comment.permalink))
                     already_checked.append(comment.permalink)  # add comment to already_checked
