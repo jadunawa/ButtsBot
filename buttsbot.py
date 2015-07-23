ea4192c31defe550e4090d795b6d8611a4f073f8
Judson Dunaway-Barlow <judson.db@gmail.com>
2015-07-23 14:58:42 -0400

    Added a remake of buttsbot to work with cron

--- buttsbot.py
+++ buttsbot.py
@@ -32,7 +32,7 @@ already_checked = []  # make list of comment permalinks
 
 #TODO: Work on regex
 #fuck regex
-keywords = ['butt', 'booty', ' ass ', 'ass.' 'keyster', 'heinie', 'hiney', 'derriere', 'posterior', 'arse', 'bottom', 'tush', 'rear', 'bum', 'caboose', 'rump', 'fanny', 'glutes']  # make list of words to trigger the comment reply
+keywords = ['butt', 'booty', ' ass ', 'ass.' 'keyster', 'heinie', 'hiney', 'derriere', 'posterior', 'arse', 'bottom', 'tush', 'rear.', ' rear ', 'rearend', 'bum', 'caboose', 'rump', 'fanny', 'glutes']  # make list of words to trigger the comment reply
 # TODO: Get a list of a bunch of imgur links to Astros Butts
 butt_links=['Bagwell butt!](http://imgur.com/Vkx6fMI.jpg', 'Castro butt!](http://www.rantsports.com/mlb/files/2014/02/Jason-Castro-Houston-Astros.jpg', 'Lowrie butt!](http://i.imgur.com/TwTi4DT.jpg',
             'Conger butt!](http://i.imgur.com/P5C2BGK.jpg', 'Carlos Lee butt!](http://i.imgur.com/G5ZzVmp.jpg', 'McHugh butt!](http://i.imgur.com/hClbvuL.jpg', 'Ausmus and Berkman butt!](http://i.imgur.com/mL5TtMx.jpg',
