d550bbd024b61225f13283260991b4f1ffed64fd
Judson Dunaway-Barlow <judson.db@gmail.com>
2016-01-28 16:46:13 -0500

    Deleted code for temporary /r/baseball threads

--- buttsbot_cron_multi.py
+++ buttsbot_cron_multi.py
@@ -41,6 +41,10 @@ r.login(un_string, pw_string, disable_warning=True)
 #fuck regex
 keywords = ['butt', 'booty', 'bootay', ' ass ',' ass.', ' ass?', ' ass,', 'asses', 'keyster', 'heinie', 'hiney', 'derriere', 'posterior', 'arse', 'bottom', 'tush', 'rear.', ' rear ', 'rearend', 'bum', 'caboose', 'rump', 'fanny', 'glutes', 'badonkadonk', 'backside']  # make list of words to trigger the comment reply
 # TODO: Get a list of a bunch of imgur links to Astros Butts
+butts_file=open(path_to_script+"/HoustonAstros.txt",'r')
+#TODO: GET FILE LENGTH AND MAKE MAP AND ASSIGN NAME AND LINK TO A MAP
+print file_len(butts_file)
+sys.exit()
 butt_links=['Bagwell butt!](http://imgur.com/Vkx6fMI.jpg', 'Castro butt!](http://www.rantsports.com/mlb/files/2014/02/Jason-Castro-Houston-Astros.jpg',
             'Lowrie butt!](http://i.imgur.com/TwTi4DT.jpg', 'Conger butt!](http://i.imgur.com/P5C2BGK.jpg', 'Carlos Lee butt!](http://i.imgur.com/G5ZzVmp.jpg',
             'McHugh butt!](http://i.imgur.com/hClbvuL.jpg','Ausmus and Berkman butt!](http://i.imgur.com/mL5TtMx.jpg',
@@ -90,53 +94,3 @@ for submission in subreddit.get_hot(limit=30):
                 conn.commit()
         print "-----------------------------------------------------------------"
     i += 1
-
-link="https://www.reddit.com/r/baseball/comments/3zugjg/astros_spotlight_2015_season_recap/"
-submission=r.get_submission(link)
-submission.replace_more_comments(limit=None, threshold=0)  # get all comments instead of just first few
-all_comments = submission.comments  # make array of comments
-flaternized_comments = praw.helpers.flatten_tree(all_comments)  # ignore tree structure of comments
-# Iterate through comments in the submission
-for comment in flaternized_comments:
-    # Ignore comments that have already been checked to avoid multiple replies to the same comment
-    perma=str(comment.permalink)
-    if (str(c.execute("SELECT link FROM permalinks WHERE link='{}'".format(perma)).fetchone())=="None"):
-        print "NONE OMG THIS WORKS"
-        l_comment = str(comment).lower()  # make the comment lowercase
-        talks_about_butts = any(string in l_comment for string in keywords)  #boolean for talking about butts
-        # If the comment talks about butts and isn't a comment by this bot, respond with the correct string
-        if talks_about_butts and str(comment.author)!="buttsbot":
-            print("Comment author: "+str(comment.author))
-            butt_number=random.randint(0,len(butt_links)-1)
-            full_reply="You have activated the Astros buttsbot! Here is a picture of [{}) Thanks for enjoying Astros buttocks! Go \'Stros!\n\nAny problems with this bot? Suggestions for more butts? Please send me a message!".format(butt_links[butt_number])
-            comment.reply(full_reply) #reply to the comment
-            comment.upvote() #upvote the comment
-            print("Replied to a comment: " +str(comment.permalink))
-        c.execute('''INSERT INTO permalinks(link) VALUES (?)''',(perma,))
-        conn.commit()
-print "-----------------------------------------------------------------"
-
-link="https://www.reddit.com/r/baseball/comments/3zugbo/astros_spotlight_season_highlights_in_gif_and/"
-submission=r.get_submission(link)
-submission.replace_more_comments(limit=None, threshold=0)  # get all comments instead of just first few
-all_comments = submission.comments  # make array of comments
-flaternized_comments = praw.helpers.flatten_tree(all_comments)  # ignore tree structure of comments
-# Iterate through comments in the submission
-for comment in flaternized_comments:
-    # Ignore comments that have already been checked to avoid multiple replies to the same comment
-    perma=str(comment.permalink)
-    if (str(c.execute("SELECT link FROM permalinks WHERE link='{}'".format(perma)).fetchone())=="None"):
-        print "NONE OMG THIS WORKS"
-        l_comment = str(comment).lower()  # make the comment lowercase
-        talks_about_butts = any(string in l_comment for string in keywords)  #boolean for talking about butts
-        # If the comment talks about butts and isn't a comment by this bot, respond with the correct string
-        if talks_about_butts and str(comment.author)!="buttsbot":
-            print("Comment author: "+str(comment.author))
-            butt_number=random.randint(0,len(butt_links)-1)
-            full_reply="You have activated the Astros buttsbot! Here is a picture of [{}) Thanks for enjoying Astros buttocks! Go \'Stros!\n\nAny problems with this bot? Suggestions for more butts? Please send me a message!".format(butt_links[butt_number])
-            comment.reply(full_reply) #reply to the comment
-            comment.upvote() #upvote the comment
-            print("Replied to a comment: " +str(comment.permalink))
-        c.execute('''INSERT INTO permalinks(link) VALUES (?)''',(perma,))
-        conn.commit()
-print "-----------------------------------------------------------------"
