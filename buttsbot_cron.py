0cdd4ca40c2e5db74bb636496147e942c5740e28
Judson Dunaway-Barlow <judson.db@gmail.com>
2016-03-31 17:08:48 -0400

    Added 4 butts, commented out trades/etc. Also did half of the unsubscribing process

--- buttsbot_cron.py
+++ buttsbot_cron.py
@@ -33,30 +33,52 @@ pw_line=config_file.readline()
 pw_string=pw_line[4:-1]
 
 #get unsubscribed users
+unsubbed_list=[]
 unsubbed_file=open(path_to_script+"/unsubscribed.txt",'r+')
+for line in unsubbed_file:
+    if line[-1:]=="\n":
+        line=line[:-1]
+    unsubbed_list.append(line)
 
 r = praw.Reddit(user_agent='I post butts!')
 r.login(un_string, pw_string, disable_warning=True)
 
 #TODO: Work on regex
 #fuck regex
+#jk regex is pretty easy lol
 keywords = ['butt', 'booty', 'bootay', 'ass', 'asses', 'keyster', 'heinie', 'hiney', 'derriere', 'posterior', 'arse', 'bottom', 'tush', 'rear', 'rearend', 'rear end', 'bum', 'caboose', 'rump', 'fanny', 'glutes', 'badonkadonk', 'backside']  # make list of words to trigger the comment reply
 # TODO: Get a list of a bunch of imgur links to Astros Butts
-butt_links=['Bagwell butt!](http://imgur.com/Vkx6fMI.jpg', 'Castro butt!](http://www.rantsports.com/mlb/files/2014/02/Jason-Castro-Houston-Astros.jpg',
-            'Lowrie butt!](http://i.imgur.com/TwTi4DT.jpg', 'Conger butt!](http://i.imgur.com/P5C2BGK.jpg', 'Carlos Lee butt!](http://i.imgur.com/G5ZzVmp.jpg',
-            'McHugh butt!](http://i.imgur.com/hClbvuL.jpg','Ausmus and Berkman butt!](http://i.imgur.com/mL5TtMx.jpg',
-            'Minor Leagues Lowrie butt!](http://i.imgur.com/VTxAaqn.jpg','Kazmir butt!](http://imgur.com/ShvX5Xq.jpg',
-            'Biggio butt!](http://i.imgur.com/7DlspmL.jpg','Biggio butt!](http://i.imgur.com/GYwHHxV.jpg','Marwin butt!](http://i.imgur.com/mqYVpy9.jpg',
-            'Julia MoralASS!](http://imgur.com/SRzPWIJ.jpg','GoGo butt!](http://i.imgur.com/Xirtvvv.jpg',
-            'Correa butt!](http://i.imgur.com/Y0JPeJK.jpg','Altuve butt!](http://i.imgur.com/bU64HWo.jpg',
-            'Biggio bobble butt!](https://i.imgur.com/I3uavEH.jpg','Lance McButt!](http://i.imgur.com/tTUpRRm.jpg',
-            'Marisnick butt! (from /u/Barrel-rider)](https://i.imgur.com/BerYpNo.jpg',
-            'Altuve on-deck butt! (from /u/Not_a_Clue)](http://i.imgur.com/gE8M0EE.jpg',
-            'A.J. Hinch butt! (from /u/thenewtestament)](http://i.imgur.com/ghZV5Lp.jpg',
-            'infamous Villar butt slide!](http://i.imgur.com/mjgWAmq.gifv','Nolan Ryan butt!](http://i.imgur.com/5MZt2w1.jpg',
-            'Gattis butt!](http://i.imgur.com/coT3SkI.jpg', 'El Oso Booty!](http://i.imgur.com/BXdprcQ.jpg']
+butt_links=['Bagwell butt!](http://imgur.com/Vkx6fMI.jpg',
+            'Castro butt!](http://www.rantsports.com/mlb/files/2014/02/Jason-Castro-Houston-Astros.jpg',
+            #'Lowrie butt!](http://i.imgur.com/TwTi4DT.jpg',
+            #'Conger butt!](http://i.imgur.com/P5C2BGK.jpg',
+            'Carlos Lee butt!](http://i.imgur.com/G5ZzVmp.jpg',
+            'McHugh butt!](http://i.imgur.com/hClbvuL.jpg',
+            'Ausmus and Berkman butt!](http://i.imgur.com/mL5TtMx.jpg',
+            #'Minor Leagues Lowrie butt!](http://i.imgur.com/VTxAaqn.jpg',
+            #'Kazmir butt!](http://imgur.com/ShvX5Xq.jpg',
+            'Biggio butt!](http://i.imgur.com/7DlspmL.jpg',
+            'Biggio butt!](http://i.imgur.com/GYwHHxV.jpg',
+            'Marwin butt!](http://i.imgur.com/mqYVpy9.jpg',
+            'Julia MoralASS!](http://imgur.com/SRzPWIJ.jpg',
+            'GoGo butt!](http://i.imgur.com/Xirtvvv.jpg',
+            'Correa butt!](http://i.imgur.com/Y0JPeJK.jpg',
+            'Altuve butt!](http://i.imgur.com/bU64HWo.jpg',
+            'Biggio bobble butt!](https://i.imgur.com/I3uavEH.jpg',
+            'Lance McButt!](http://i.imgur.com/tTUpRRm.jpg',
+            'Marisnick butt! (photo credit: /u/Barrel-rider)](https://i.imgur.com/BerYpNo.jpg',
+            'Altuve on-deck butt! (photo credit: /u/Not_a_Clue)](http://i.imgur.com/gE8M0EE.jpg',
+            'A.J. Hinch butt! (photo credit: /u/thenewtestament)](http://i.imgur.com/ghZV5Lp.jpg',
+            'infamous Villar butt slide!](http://i.imgur.com/mjgWAmq.gifv',
+            'Nolan Ryan butt!](http://i.imgur.com/5MZt2w1.jpg',
+            'Gattis butt!](http://i.imgur.com/coT3SkI.jpg',
+            'El Oso Booty!](http://i.imgur.com/BXdprcQ.jpg',
+            'Doug Fister butt!](http://i.imgur.com/u6I8Rn9.jpg',
+            'Ken Giles butt!](http://i.imgur.com/IJu2wI3.jpg',
+            'Cy Young winning butt!](http://i.imgur.com/NTolVAP.jpg'
+            'Tony Sipp butt!](http://i.imgur.com/n5opLnu.jpg']
 
-subreddit = r.get_subreddit('Astros+AstrosCirclejerk')  # get /r/Astros
+subreddit = r.get_subreddit('Astros+AstrosCirclejerk+ButtsBot')  # get /r/Astros
 #subreddit = r.get_subreddit('Astros+Mariners') they didn't like me here either :(
 #subreddit = r.get_subreddit('Astros+TexasRangers') #temporary surprise... jk I got banned :(
 
@@ -81,23 +103,21 @@ for submission in subreddit.get_hot(limit=30):
                 l_comment = str(comment).lower()  # make the comment lowercase
                 #talks_about_butts = any(string in l_comment for string in keywords)
                 talks_about_butts=False # #boolean for talking about butts
-                #TODO: INSERT REGEX HERE
-                found_using_regex=False
                 for keyword in keywords:
-                    regex_string=r"\b"+keyword+r"e*s*\b"
+                    regex_string=r"\b"+keyword+r"i*e*s*\b"
                     search_result=re.search(regex_string, str(comment), re.IGNORECASE)
                     #print search_result
-                    BETA_STRING="This comment was made using BB beta! Please ignore this, this line is only used for testing."
                     if(search_result):
                         talks_about_butts=True
                         print 'keyword '+keyword+" worked. Comment author was "+str(comment.author)
                         print "permalink: "+perma
                         #print "TRUE: "+ keyword+"\ncomment: "+perma+"\nauthor:"+str(comment.author)
                 # If the comment talks about butts and isn't a comment by this bot, respond with the correct string
-                if talks_about_butts and str(comment.author)!="buttsbot":
+                #TODO: check for unsubscribed users
+                if talks_about_butts and str(comment.author)!="buttsbot" and str(comment.author) not in unsubbed_list:
                     print("Comment author: "+str(comment.author))
                     butt_number=random.randint(0,len(butt_links)-1)
-                    full_reply="You have activated the Astros buttsbot! Here is a picture of [{}) Thanks for enjoying Astros buttocks! Go \'Stros!\n\nAny problems with this bot? Suggestions for more butts? Please send me a message!".format(butt_links[butt_number])
+                    full_reply="You have activated the Astros buttsbot! Here is a picture of [{}) Thanks for enjoying Astros buttocks! Go \'Stros!\n\nAny problems with this bot? Suggestions for more butts? Please send me a message or visit /r/ButtsBot!".format(butt_links[butt_number])
                     comment.reply(full_reply) #reply to the comment
                     comment.upvote() #upvote the comment
                     print("Replied to a comment: " +str(comment.permalink))
