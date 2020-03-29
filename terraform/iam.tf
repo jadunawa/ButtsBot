4d3b866642d9d1ff667a17cf1446b2458a817164
Judson Dunaway-Barlow <judson.dunawaybarlow@asics.com>
2020-03-29 15:05:22 -0400

    adding tf_cloudwatch_buttsbot_event_target_role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -12,3 +12,20 @@ resource "aws_iam_role" "tf_container_job_role" {
     }
 EOF
 }
+
+resource "aws_iam_role" "tf_cloudwatch_buttsbot_event_target_role" {
+  name = "tf_cloudwatch_buttsbot_event_target_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version":"2012-10-17",
+  "Statement":[{
+    "Effect":"Allow",
+    "Principal":{
+      "Service":"events.amazonaws.com"
+    },
+    "Action":"sts:AssumeRole"
+  }]
+}
+EOF
+}
